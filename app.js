10f7f79dfe249698bd2466dddfac112836fd2bae
Nox <benaziz.ines@gmail.com>
2022-03-15 18:23:11 +0100

    iteration 1 done

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
-});
+})
+
+app.get('/beers', (req, res) => {
+  res.render('index')
+})
+
+app.get('/randombeers', (req, res) => {
+  res.render('index')
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
